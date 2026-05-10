d41571ac73ee00cdd445d2bb6f7dd96e8316d719
Philip <philiplambok@gmail.com>
2026-05-10 17:06:24 +0700

    fix: use DROP COLUMN IF EXISTS to handle fresh database migrations

--- db/migrations/20260503000005_drop_users_avatar_url.sql
+++ db/migrations/20260503000005_drop_users_avatar_url.sql
@@ -1,5 +1,5 @@
 -- +goose Up
-ALTER TABLE users DROP COLUMN avatar_url;
+ALTER TABLE users DROP COLUMN IF EXISTS avatar_url;
 
 -- +goose Down
 ALTER TABLE users ADD COLUMN avatar_url TEXT NOT NULL DEFAULT '';
